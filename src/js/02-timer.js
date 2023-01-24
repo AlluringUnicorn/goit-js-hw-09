92c5cb4b79403d43930b7c2987f174ae1bec4a78
Mariia Khapun <mariakhapun@gmail.com>
2023-01-24 12:01:57 +0000

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,15 +3,14 @@ import 'flatpickr/dist/flatpickr.min.css';
 
 const input = document.getElementById('datetime-picker');
 
-const date = new Date();
-console.log(date);
+console.log('Hello World!!!!!');
 
-flatpickr(input, {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  },
-});
+// flatpickr(input, {
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//   onClose(selectedDates) {
+//     console.log(selectedDates[0]);
+//   },
+// });
