242e1925956302a2ea318dbe1959a4e8dead15e1
Mariia Khapun <mariakhapun@gmail.com>
2023-01-24 15:11:43 +0000

    #2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,15 +1,76 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import 'notiflix/dist/notiflix-3.2.6.min.css';
 
 const input = document.getElementById('datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+const daysSpan = document.querySelector('[data-days]');
+const hoursSpan = document.querySelector('[data-hours]');
+const minutesSpan = document.querySelector('[data-minutes]');
+const secondsSpan = document.querySelector('[data-seconds]');
 
-// flatpickr(input, {
-//   enableTime: true,
-//   time_24hr: true,
-//   defaultDate: new Date(),
-//   minuteIncrement: 1,
-//   onClose(selectedDates) {
-//     console.log(selectedDates[0]);
-//   },
-// });
-console.log('Hello');
+flatpickr(input, {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    chooseDate(selectedDates);
+  },
+});
+
+startBtn.addEventListener('click', startTimer);
+
+function startTimer() {
+  const intervalId = setInterval(() => {
+    const differenceMs = new Date(input.value) - Date.now();
+    const difference = convertMs(differenceMs);
+
+    if (differenceMs < 0) {
+      return;
+    }
+    updateClock(difference);
+  }, 1000);
+}
+
+function updateClock({ days, hours, minutes, seconds }) {
+  daysSpan.textContent = addLeadingZero(days);
+  hoursSpan.textContent = addLeadingZero(hours);
+  minutesSpan.textContent = addLeadingZero(minutes);
+  secondsSpan.textContent = addLeadingZero(seconds);
+}
+
+function chooseDate(selectedDates) {
+  const currentDate = new Date().getTime();
+
+  if (selectedDates[0].getTime() > currentDate) {
+    startBtn.disabled = false;
+  } else {
+    console.log('Date in past');
+    Notify.failure('Please choose a date in the future');
+  }
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
